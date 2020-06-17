00755e96d35f76f5ebef8d1c66591346e0738c68
John Stuppy <stuppy.john@gmail.com>
2020-06-17 10:56:24 -0500

    -turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//  Note: Removed turbolinks.
+//
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
