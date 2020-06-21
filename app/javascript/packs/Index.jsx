59cef81d9ded20e08e542a8619fd57d4e34d0886
John Stuppy <stuppy.john@gmail.com>
2020-06-21 11:47:54 -0500

    reorder

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,8 +1,7 @@
+import App from "../components/App";
 import React from "react";
 import { render } from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import App from "../components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
