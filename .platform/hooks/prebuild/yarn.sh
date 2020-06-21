20db15e20e5879493bb288d2e5b2a2a8287d7fe6
John Stuppy <stuppy.john@gmail.com>
2020-06-21 07:53:24 -0500

    Update yarn.sh

--- .platform/hooks/prebuild/yarn.sh
+++ .platform/hooks/prebuild/yarn.sh
@@ -3,11 +3,15 @@
 #!/bin/bash
 
 # need to install node first to be able to install yarn (as at prebuild no node is present yet)
+echo "download/setup nodejs..."
 sudo curl --silent --location https://rpm.nodesource.com/setup_12.x | sudo bash -
+echo "yum nodejs..."
 sudo yum -y install nodejs
 
 # install yarn
+echo "get yarn..."
 sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+echo "yum yarn..."
 sudo yum -y install yarn
 
 # install
@@ -16,6 +20,7 @@ cd /var/app/staging/
 # debugging..
 ls -lah
 
+echo "yarn install..."
 yarn install --prod
 
 chown -R webapp:webapp node_modules/ || true # allow to fail
